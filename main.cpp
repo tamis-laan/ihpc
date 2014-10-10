0aa598577e80586eeeb699a072467e34b6ec1df3
Jasper Ruoff <jruoff@gmail.com>
2014-10-10 13:53:18 +0200

    MPI Hello world!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <mpi/mpi.h>
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello, world!" << std::endl;
+	MPI_Init(&argc, &argv);
+	
+	int np, rank;
+	
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+	std::cout << "Node " << rank << " of " << np << " says: Hello world!" << std::endl;
+	
+	MPI_Finalize();
+	return 0;
 }
